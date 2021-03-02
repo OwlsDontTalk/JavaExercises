c10383d0c241d96c037786ab7ea111876ef8f1a4
Ivan Petrov <petrovodt@gmail.com>
2021-03-03 01:22:37 +0300

    spring boot module init

---
+++ springboot/src/main/java/com/owlsdonttalk/SpringbootApplication.java
@@ -0,0 +1,26 @@
+package com.owlsdonttalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ //TODO
+    0. Мигрировать JavaExercisesSpring и избавиться от лишнего проетка
+    1. Поиграться со streamapi :: создаем лист хм :: и разные имитации add, remove, update user, Только через streamapi
+    2. Разобраться как работает база :: прикруть sqlite :: hibernate
+    3. Поиграться с сохранением данных, например юзерей
+    4. Видосы с ютуба
+    5. Autowire?
+
+ *
+ */
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
---
+++ springboot/src/main/java/com/owlsdonttalk/presist/User.java
@@ -0,0 +1,44 @@
+package com.owlsdonttalk.presist;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
